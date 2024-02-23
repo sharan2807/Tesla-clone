37cc4076b3980f22fb73d9fd9614e937db62fc5e
sharan2807 <sharancold98@gmail.com>
2024-02-23 17:03:45 +0530

    Adding the Logo with fixed styling

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <Container>
+      <a>
+        <img src="images/logo.svg" />
+      </a>
+      <Menu></Menu>
+    </Container>
+  );
 }
 
 export default Header;
+
+const Container = styled.div`
+  min-height: 60px;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  padding: 0 20px;
+`;
+const Menu = styled.div``;
