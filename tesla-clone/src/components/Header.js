ce1988024dd2519f96306dba822cc3f08869780f
sharan2807 <sharancold98@gmail.com>
2024-02-23 17:12:20 +0530

    Completed <Menu> comp.

--- tesla-clone/src/components/Header.js
+++ tesla-clone/src/components/Header.js
@@ -33,5 +33,16 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   padding: 0 20px;
+  top: 0;
+  left: 0;
+  right: 0;
 `;
-const Menu = styled.div``;
+const Menu = styled.div`
+  display: flex;
+  align-items: center;
+  p {
+    font-weight: bold;
+    text-transform: uppercase;
+    padding: 0 10px;
+  }
+`;
