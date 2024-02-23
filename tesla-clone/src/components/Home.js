81f92d155546a4b4c4e59ca1cfc34eea56f0fdea
sharan2807 <sharancold98@gmail.com>
2024-02-23 16:52:11 +0530

    Making section component dynamic & adding multiple Section Components

--- tesla-clone/src/components/Home.js
+++ tesla-clone/src/components/Home.js
@@ -12,6 +12,48 @@ function Home() {
         rightBtnText="Existing Inventory"
         backgroundImg="model-s.jpg"
       />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-y.jpg"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-x.jpg"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        leftBtnText="Custom Order"
+        rightBtnText="Existing Inventory"
+        backgroundImg="model-3.jpg"
+      />
+      <Section
+        title="Lowest Cost Solar Panels In America"
+        description="Money-back-guarantee"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+        backgroundImg="solar-panel.jpg"
+      />
+      <Section
+        title="Solar for New Roofs"
+        description="Solar Roofs Costs Less Than a New Roof Plus Solar Panels"
+        leftBtnText="Order Now"
+        rightBtnText="Learn More"
+        backgroundImg="solar-roof.jpg"
+      />
+      <Section
+        title="Accessories"
+        description=""
+        leftBtnText="Shop Now"
+        rightBtnText=""
+        backgroundImg="accessories.jpg"
+      />
     </Container>
   );
 }
